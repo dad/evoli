8c11e8ad8efb1fad516f5557d89c6220552734eb
wilke <wilke@dadfaeae-b11a-0410-95f2-80a8e7ca9aa5>
2006-08-15 03:48:37 +0000

    Minor build fixes. Python build outside of src directory should work now.
    git-svn-id: http://wlab.ccbb.utexas.edu/svn/evoli@55 dadfaeae-b11a-0410-95f2-80a8e7ca9aa5

--- trunk/src/python/setup.py
+++ trunk/src/python/setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup, Extension
-import os
+import os, sys
+
+srcdir = sys.argv.pop()
+print "top_srcdir =", srcdir
+builddir = sys.argv.pop()
+print "top_builddir =", builddir
 
 folder = Extension('folder',
 				   sources = ['foldermodule.cc'],
-				   include_dirs = ['../folder'],
-				   libraries = ['folder'],
-				   library_dirs = ['../folder'])
+				   include_dirs = [srcdir + '/src/folder', srcdir + '/src/evolver'],
+				   libraries = ['folder', 'evolver'],
+				   library_dirs = [srcdir + '/src/folder', srcdir + '/src/evolver'])
 
 setup (name = 'Folder',
 	          version = '0.1',
