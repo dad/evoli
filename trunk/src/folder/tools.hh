3a548b30a6916a013cd00be9dd7946d66d24ec07
dad <dad@dadfaeae-b11a-0410-95f2-80a8e7ca9aa5>
2006-08-31 21:06:30 +0000

    Added itoa to tools.hh
    git-svn-id: http://wlab.ccbb.utexas.edu/svn/evoli@95 dadfaeae-b11a-0410-95f2-80a8e7ca9aa5

--- trunk/src/folder/tools.hh
+++ trunk/src/folder/tools.hh
@@ -174,6 +174,24 @@ inline pair<double, double> meanvar( double s1, double s2, int n )
         return p;
 }
 
+inline string itoa(int value, int base) {
+	enum { kMaxDigits = 35 };
+	string buf;
+	buf.reserve( kMaxDigits ); // Pre-allocate enough space.
+	// Check that the base is valid
+	if (base < 2 || base > 16) return buf;
+	int quotient = value;
+	// Translating number to string with base:
+	do {
+		buf += "0123456789abcdef"[ std::abs( quotient % base ) ];
+		quotient /= base;
+	} while ( quotient );
+	// Append the negative sign for base 10
+	if ( value < 0 && base == 10) buf += '-';
+	reverse( buf.begin(), buf.end() );
+	return buf;
+}
+
 // The following is taken from http://www.herine.net/stat/papers/dbinom.pdf
 // the Loader algorithm for fast, accurate computation of binomial probabilities.
 
